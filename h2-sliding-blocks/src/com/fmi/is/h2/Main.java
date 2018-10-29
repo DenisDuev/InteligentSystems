d8370e96903ebada598e6c1e8e9be4185d42ddc0
Denis Duev <denis_duev@abv.bg>
2018-10-29 23:52:51 +0200

    fix algorithm

--- h2-sliding-blocks/src/com/fmi/is/h2/Main.java
+++ h2-sliding-blocks/src/com/fmi/is/h2/Main.java
@@ -5,8 +5,9 @@ import java.util.*;
 public class Main {
 
     static Board board;
-    static Set<Board> visited = new HashSet<>();
+    static Set<String> visited = new HashSet<>();
     static Stack<String> path = new Stack<>();
+    static TreeMap<Integer, Action> map = new TreeMap<>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -17,63 +18,62 @@ public class Main {
 
         }
         board = new Board(entries);
-        search(board.getEmptyPoint(), board);
+        search(board.getEmptyPoint(), board, 0);
         System.out.println(path.size());
         while (!path.empty()) {
             System.out.println(path.pop());
         }
     }
 
-    public static boolean search(Point starting, Board board) {
-        if (visited.contains(board)) {
+    public static boolean search(Point starting, Board board, int distance) {
+        if (visited.contains(board.toString())) {
             return false;
         }
-        visited.add(board);
+        visited.add(board.toString());
 
         if (board.getDistanceSum() == 0) {
             return true;
         }
 
-        TreeMap<Integer, Action> map = new TreeMap<>();
         Board upBoard = new Board(board);
         upBoard.moveUp();
-        map.put(upBoard.getDistanceSum(), Action.UP);
-        Board downBoard= new Board(board);
+        map.put(upBoard.getDistanceSum() + distance, Action.UP);
+        Board downBoard = new Board(board);
         downBoard.moveDown();
-        map.put(downBoard.getDistanceSum(), Action.DOWN);
-        Board leftBoard= new Board(board);
+        map.put(downBoard.getDistanceSum() + distance, Action.DOWN);
+        Board leftBoard = new Board(board);
         leftBoard.moveLeft();
-        map.put(leftBoard.getDistanceSum(), Action.LEFT);
-        Board rightBoard= new Board(board);
+        map.put(leftBoard.getDistanceSum() + distance, Action.LEFT);
+        Board rightBoard = new Board(board);
         rightBoard.moveRight();
-        map.put(rightBoard.getDistanceSum(), Action.RIGHT);
+        map.put(rightBoard.getDistanceSum() + distance, Action.RIGHT);
 
-        List<Map.Entry<Integer, Action>> list = new ArrayList<>(map.entrySet());
-        for (Map.Entry<Integer, Action> entry : list) {
+        while (map.size() > 0){
+            Map.Entry<Integer, Action> entry = map.pollFirstEntry();
             if (entry.getKey() == -1) {
                 return false;
             }
             switch (entry.getValue()) {
                 case UP:
-                    if (search(new Point(starting.getRow() - 1, starting.getCol()), upBoard)) {
+                    if (search(new Point(starting.getRow() - 1, starting.getCol()), upBoard, distance + 1)) {
                         path.push("up");
                         return true;
                     }
                     break;
                 case DOWN:
-                    if (search(new Point(starting.getRow() + 1, starting.getCol()), downBoard)) {
+                    if (search(new Point(starting.getRow() + 1, starting.getCol()), downBoard, distance + 1)) {
                         path.push("down");
                         return true;
                     }
                     break;
                 case LEFT:
-                    if (search(new Point(starting.getRow(), starting.getCol() + 1), leftBoard)) {
+                    if (search(new Point(starting.getRow(), starting.getCol() + 1), leftBoard, distance + 1)) {
                         path.push("left");
                         return true;
                     }
                     break;
                 case RIGHT:
-                    if (search(new Point(starting.getRow(), starting.getCol() - 1), rightBoard)) {
+                    if (search(new Point(starting.getRow(), starting.getCol() - 1), rightBoard, distance + 1)) {
                         path.push("right");
                         return true;
                     }
